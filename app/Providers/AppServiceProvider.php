1c233f5be583384dcfa23b080c6c6a260d34f2a4
Ilgar <eliyev.iq@gmail.com>
2020-05-09 14:44:54 +0400

    new

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        view()->share('categoires',Category::get());
-        view()->share('pposts',Post::get());
+        
+        
     }
 }
